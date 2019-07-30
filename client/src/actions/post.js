09c6d73ffbd42b7e196636a25815eef249c6d918
Jo <jo.mtz133@gmail.com>
2019-07-30 18:52:18 -0500

    Adding posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {setAlert} from "./alert";
 import {
+    ADD_POST,
     DELETE_POST,
     GET_POSTS,
     POST_ERROR,
@@ -64,6 +65,29 @@ export const deletePost = id => async dispatch => {
             payload: id
         });
         dispatch(setAlert('Post removed', 'success'))
+    }catch (e) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {msg: e.response.statusText, status: e.response.status}
+        });
+    }
+};
+
+
+// Add post
+export const addPost = formData => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    try {
+        const res = await axios.post(`/api/posts`, formData);
+        dispatch({
+            type: ADD_POST,
+            payload: res.data
+        });
+        dispatch(setAlert('Post Created', 'success'))
     }catch (e) {
         dispatch({
             type: POST_ERROR,
