2632a4169c82af88bfe7267efb973d030579ebe0
AbrahamG <abraham.olague@gmail.com>
2019-08-05 19:34:31 -0500

    landing page updated

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -11,10 +11,9 @@ const Landing = ({isAuthenticated}) => {
         <section className="landing">
             <div className="dark-overlay">
                 <div className="landing-inner">
-                    <h1 className="x-large">Developer Connector</h1>
+                    <h1 className="x-large">f a q B o x</h1>
                     <p className="lead">
-                        Create a developer profile/portfolio, share posts and get help from
-                        other developers
+                        Manage your Organization's Process Database, Create Documentation and Answer F.A.Q
                     </p>
                     <div className="buttons">
                         <Link to="/register" className="btn btn-primary">Sign Up</Link>
